d638a2deb44bc826a49248c0791c7429c7aeffca
ayada20 <edaayada@gmail.com>
2025-10-16 12:58:53 +0200

    feat(ex9):exercise 9 finished

---
+++ app/composables/useCounter.ts
@@ -0,0 +1,16 @@
+export function useCounter() {
+  const count = ref(0);
+
+  function increment() {
+    count.value++;
+  }
+
+  function decrement() {
+    count.value--;
+  }
+
+  function reset() {
+    count.value = 0;
+  }
+  return { count, increment, decrement, reset };
+}
---
+++ app/pages/exercise9.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { count, increment, decrement, reset } = useCounter();
+</script>
+
+<template>
+  <div>
+    <button @click="increment">Incrementar</button>
+    <button @click="decrement">Decrememntar</button>
+    <button @click="reset">Reiniciar</button>
+    <p>valor: {{ count }}</p>
+  </div>
+</template>
