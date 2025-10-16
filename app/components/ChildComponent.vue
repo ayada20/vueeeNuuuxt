71a367a02a3e2b80b9dc6341062eb53066a7a282
ayada20 <edaayada@gmail.com>
2025-10-16 12:06:22 +0200

    feat(ex8):exercise8 completed

---
+++ app/components/ChildComponent.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const msg = inject("message");
+</script>
+
+<template>
+  <div>{{ msg }}</div>
+</template>
---
+++ app/components/ParentComponent.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+provide("message", "hola desde el componente padre al hijo");
+</script>
+
+<template>
+  <div><ChildComponent /></div>
+</template>
