7fe8307f6c3399d4776443ba25525a59bd760204
ayada20 <edaayada@gmail.com>
2025-10-17 09:37:57 +0200

    feat(ex13):exercise13 finished

--- app/components/TheCounter.vue
+++ app/components/TheCounter.vue
@@ -4,14 +4,28 @@ const countPorDos = computed(() => count.value * 2);
 
 const incrementar = () => (count.value += 1);
 const decrementar = () => (count.value -= 1);
+
+const message = ref("m");
+watch(count, (valorNuevo) => {
+  if (valorNuevo === 0) {
+    message.value = "estas en el valor minimo;";
+  } else if (valorNuevo === 10) {
+    message.value = "estas en el valor maximo";
+  } else if (valorNuevo >= 0 && valorNuevo <= 10) {
+    message.value = "estas en los parametros adecuados";
+  } else {
+    message.value = "";
+  }
+});
 </script>
 
 <template>
   <div>
-    <h1>Counter 2</h1>
-    <p>{{ countPorDos }}</p>
+    <h1>Counter 3 watchers</h1>
+    <p>{{ count }}</p>
 
     <button @click="incrementar">incrementar</button>
-    <button @click="decrementar">incrementar</button>
+    <button @click="decrementar">decrementar</button>
+    <p>{{ message }}</p>
   </div>
 </template>
