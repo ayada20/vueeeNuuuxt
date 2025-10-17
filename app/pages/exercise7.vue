7d6000096e5b254c380edc3c53a1fb0e06f397f0
ayada20 <edaayada@gmail.com>
2025-10-17 08:42:28 +0200

    feat(ex7):exercise7 finished

--- app/pages/exercise7.vue
+++ app/pages/exercise7.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-import ParentComponent from "@/pages/components/ParentComponent.vue";
-</script>
+<script setup lang="ts"></script>
 <template>
   <div>
     <ParentComponent />
